181580e1c705cbbac2a70cebcb800d2bf667f46e
Amos <igefad12@gmail.com>
2026-04-21 23:30:55 +0100

    UPDATE: index.php entry

--- public/index.php
+++ public/index.php
@@ -1,10 +1,12 @@
 <?php
 
 // Setup Constants & Autoload
+define('PUBLIC_PATH', __DIR__);
 define('ROOT_PATH', dirname(__DIR__));
+
 require ROOT_PATH . '/vendor/autoload.php';
 
 // Savv handles the Constants, Dotenv, and Routing internally
-$app = \Savv\Core\Application::bootstrap(dirname(__DIR__));
+$app = \Savv\Core\Application::bootstrap(ROOT_PATH);
 
 $app->run();
